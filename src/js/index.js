a047a375cb4acf33b11f17a191a938e99645bfc0
soran <soranlotfi2023@gmail.com>
2024-06-26 02:38:37 +0330

    render sorted transactions

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,13 @@ const transactionsLoad = document.querySelector(".show-transactions-btn")
 const transactionsLoadContainer = document.querySelector(".show-transactions-button-container")
 const transactionsTable = document.querySelector(".table-container")
 const transactionsTablesBody = document.querySelector(".transactions-table-body")
+const priceSortIcon = document.querySelector(".price-sort-icon")
+const dateSortIcon = document.querySelector(".date-sort-icon")
+const searchInput = document.querySelector(".search-input")
+const searchIcon = document.querySelector(".search-icon")
+
+
+let searchParams={}
 // 1.show transactions using btn
 transactionsLoad.addEventListener("click" , async ()=>{
     transactionsLoadContainer.style.display="none"
@@ -12,9 +19,26 @@ transactionsLoad.addEventListener("click" , async ()=>{
     ui.renderTransactions(transactions.data)
 
 })
-
-
-
+// do some moves on the icons
+priceSortIcon.addEventListener("click" , ()=>{
+    priceSortIcon.classList.toggle("rotate")
+    const api = new Api()
+    if(priceSortIcon.classList.contains("rotate")) api.filterTransaction({priceSort:"asc"})
+    else api.filterTransaction({priceSort:"desc"})
+})
+dateSortIcon.addEventListener("click" , ()=>{
+    const api = new Api()
+    dateSortIcon.classList.toggle("rotate")
+    if(dateSortIcon.classList.contains("rotate")) api.filterTransaction({dateSort:"asc"})
+    else api.filterTransaction({dateSort:"desc"})
+})
+searchInput.addEventListener("focus" , ()=>{
+    !searchIcon.classList.contains("search-animate") ? searchIcon.classList.add("search-animate") : searchIcon.classList.remove("search-animate")
+})
+searchInput.addEventListener("input" , (e)=>{
+    const api = new Api()
+    api.filterTransaction({refId:e.target.value})
+})
 class Ui{
     renderTransactions(transactions){
         console.log(transactions)
@@ -34,20 +58,37 @@ class Ui{
         transactionsTablesBody.innerHTML=result
     }
 }
-
-
 class Api{
      baseUrl="http://localhost:3000";
     async getTransactions(){
         try {
-            return  await axios.get(`${this.baseUrl}/transactions`)
+            return  await axios.get(" http://localhost:3000/transactions")
         }
         catch (e){
-            console.log(e.message)
+            throw new Error(e.message)
         }
      }
-}
 
+    async  filterTransaction({ refId = "", dateSort = "", priceSort = "" }) {
+        let url = `${this.baseUrl}/transactions?`;
+
+        if (refId) searchParams.refId=`refId_like=${refId}`;
+        if (priceSort) searchParams.sort=`&_sort=price&_order=${priceSort}`
+        if (dateSort)searchParams.sort=`&_sort=date&_order=${dateSort}` ;
+
+        if(searchParams.refId) url+=searchParams.refId ?? ""
+        if(searchParams.sort) url+=searchParams.sort ?? ""
+        if(searchParams.dateSort) url+=searchParams.dateSort ?? ""
+
+        try {
+            const filteredData = await axios.get(url);
+            const ui = new Ui();
+            ui.renderTransactions(filteredData.data);
+        } catch (e) {
+            throw new Error(e.message);
+        }
+    }
+}
 class Date{
     static options = {
             year: "numeric",
@@ -58,11 +99,7 @@ class Date{
             minute:"numeric"
     }
     static dateConvert(transactionDate){
-        console.log(transactionDate)
-        const localDate = new Date(transactionDate)
         const convertedDate = (Intl.DateTimeFormat("fa-IR" , Date.options).format(transactionDate))
-
-        /*console.log(date)*/
         return `${convertedDate}`
     }
 }
